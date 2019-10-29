10cad5134f458d64fafbca60fb90ebdf8f8a2738
Luis Toro <uk.luistoro@gmail.com>
2019-10-29 09:41:21 +0000

    Model object added
    Model Object  will have properties of the game. It will also have the logic to find out if there is a hit or miss in the game.

--- battleship.js
+++ battleship.js
@@ -1,6 +1,12 @@
-var view = {
+// ---------------------------------
+//           VIEW OBJECT
+// ---------------------------------
+// It will display when there is a hit, miss on the grid.
+// It will display a message to the user when a hit, miss, ship gets sunk and when the battleship gets sunk.
+
+const view = {
     displayMessage: function (msg) {
-        const message = document.getElementById("messageArea");
+        let message = document.getElementById("messageArea");
         message.innerHTML = msg;
     },
     displayHit: function (locations) {
@@ -12,6 +18,46 @@ var view = {
         miss.setAttribute("class", "miss");
     }
 }
-view.displayMiss("00")
-view.displayHit("01")
-view.displayMessage("HELLO WORLD")
+
+
+
+// ---------------------------------
+//           MODEL OBJECT
+// ---------------------------------
+// It has properties for the game.
+// It has the logic to find out if there is a Hit or Miss.
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    
+    ships: [ // ships locations in the grid        
+        {locations: ["00","01","02"], hit: ["","",""]},
+        {locations: ["10","11","12"], hit: ["","",""]},
+        {locations: ["20","21","22"], hit: ["","",""]}
+    ],
+
+    fire: function (location) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(location); // if it matches a location return the location number, otherwise return -1 //
+            
+            if (index >= 0){
+                // we have a hit
+                ship.hit[index] = "hit"; // hit class added //
+                view.displayHit(location);
+                view.displayMessage("HIT");
+                return true;
+            }
+        }
+        // we have a miss
+        view.displayMiss(location);
+        view.displayMessage("MISSED"); // miss class added //
+        return false;
+    }
+}
+
+model.fire("10")
+model.fire("30")
